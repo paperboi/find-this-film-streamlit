d666015f2390735d434c7069c32f16d86451af48
paperboi <jeffreysamjacob@gmail.com>
2023-12-18 19:47:59 +0530

    added json download button

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from movie_details import fetch_movie_details, display_movie_details
+import json
 
 ALLOWED_EXTENSIONS = {'txt', 'csv'}  # Add the allowed file extensions here
 
@@ -28,8 +29,22 @@ def main():
             st.header("Results")
             for result in results:
                 display_movie_details(result)
+            # Add a button to download the results as JSON
+            download_button = st.download_button(
+                label="Download results as JSON",
+                data=json.dumps(results, indent=2),
+                file_name="movie_details_results.json",
+                key="download_button"
+            )
         else:
             result = fetch_movie_details(film_name, year_of_release)
+            # Add a button to download the result as JSON
+            download_button = st.download_button(
+                label="Download results as JSON",
+                data=json.dumps(result, indent=2),
+                file_name="movie_details_results.json",
+                key="download_button"
+            )
 
             if 'error' in result:
                 st.error(result['error'])
