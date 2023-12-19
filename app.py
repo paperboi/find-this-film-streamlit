2e3beed9f6d7f77964cf747e0a3d87b236c52802
paperboi <jeffreysamjacob@gmail.com>
2023-12-19 11:21:44 +0530

    added download as csv button

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from movie_details import fetch_movie_details, display_movie_details
+import pandas as pd
 import json
 
 ALLOWED_EXTENSIONS = {'txt', 'csv'}  # Add the allowed file extensions here
@@ -29,27 +30,51 @@ def main():
             st.header("Results")
             for result in results:
                 display_movie_details(result)
+            
             # Add a button to download the results as JSON
             download_button = st.download_button(
                 label="Download results as JSON",
                 data=json.dumps(results, indent=2),
                 file_name="movie_details_results.json",
-                key="download_button"
+                key="download_json_button"
+            )
+            
+            # Convert the results to a DataFrame
+            df_results = pd.DataFrame(results)
+
+            # Add a button to download the results as CSV
+            download_csv_button = st.download_button(
+                label="Download results as CSV",
+                data=df_results.to_csv(index=False),
+                file_name="movie_details_results.csv",
+                key="download_csv_button"
             )
         else:
             result = fetch_movie_details(film_name, year_of_release)
-            # Add a button to download the result as JSON
-            download_button = st.download_button(
-                label="Download results as JSON",
-                data=json.dumps(result, indent=2),
-                file_name="movie_details_results.json",
-                key="download_button"
-            )
 
             if 'error' in result:
                 st.error(result['error'])
             else:
                 display_movie_details(result)
+                
+                # Add a button to download the result as JSON
+                download_button = st.download_button(
+                    label="Download results as JSON",
+                    data=json.dumps(result, indent=2),
+                    file_name="movie_details_results.json",
+                    key="download_json_button"
+                )
+                
+                # Convert the results to a DataFrame
+                df_result = pd.DataFrame(result)
+
+                # Add a button to download the results as CSV
+                download_csv_button = st.download_button(
+                    label="Download results as CSV",
+                    data=df_result.to_csv(index=False),
+                    file_name="movie_details_results.csv",
+                    key="download_csv_button"
+                )
 
 if __name__ == "__main__":
     main()
